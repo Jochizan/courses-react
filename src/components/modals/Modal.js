039a3908a1e534ee16cd43f63bd018677acd1888
Joan <69653003+Jochizan@users.noreply.github.com>
2021-05-03 16:49:47 -0500

    Adding changes and stopPropagation event

--- src/components/modals/Modal.js
+++ src/components/modals/Modal.js
@@ -1,9 +1,12 @@
 import './Modal.css';
 
 const Modal = ({ children, isOpen, handleClose }) => {
+  const handleModalContainerClick = (e) => e.stopPropagation();
+
   return (
-    <div className={`modal ${isOpen ? 'is-open' : ''}`}>
-      <div className='modal-container'>
+    <div className={`modal ${isOpen && 'is-open'}`} onClick={handleClose}>
+      <div className='modal-container' onClick={handleModalContainerClick}>
+        {/* le agrego el stop propagation porque sino seria como si al hacer click al modal-container tambien le haría click al div principal con la clase modal y cerraria todo */}
         <button className='modal-close' onClick={handleClose}>
           X
         </button>
--- src/components/modals/Modals.js
+++ src/components/modals/Modals.js
@@ -6,6 +6,8 @@ const Modals = () => {
 
   const [isOpenModal2, openModal2, closeModal2] = useModal(false);
 
+  const [isOpenContact, openModalContact, closeModalContact] = useModal(false);
+
   return (
     <div>
       <h2>Modales</h2>
@@ -14,6 +16,18 @@ const Modals = () => {
         <h3>Modal 1</h3>
         <p>Este es el contenido de mi modal 1</p>
         <img src='https://placeimg.com/400/400/animals' alt='Animals' />
+      </Modal>
+      <h2>Modales</h2>
+      <button onClick={openModal2}>Modal 2</button>
+      <Modal isOpen={isOpenModal2} handleClose={closeModal2}>
+        <h3>Modal 2</h3>
+        <p>Este es el contenido de mi modal 2</p>
+        <img src='https://placeimg.com/400/400/animals' alt='Animals' />
+      </Modal>
+      <h2>Modal Formulario</h2>
+      <button onClick={openModalContact}>Modal Contacto</button>
+      <Modal isOpen={isOpenContact} handleClose={closeModalContact}>
+        formulario...
       </Modal>
     </div>
   );
