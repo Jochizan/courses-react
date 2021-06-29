c337c8d6f304780284dd001df0532f7c6402bae7
Jochizan <remnyachizot2015@gmail.com>
2021-06-29 14:46:01 -0500

    Avances del curso de react con la Context API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import MyPage from './components/MyPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Context API</h1>
+      <a
+        href='https://es.react.org/docs/context.html'
+        target='_blank'
+        rel='noreferrer'
+      >
+        Documentación
+      </a>
+      <hr />
+      <MyPage />
     </div>
   );
 }
