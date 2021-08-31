2098fb5d4e24cc35ae57190007efd0dc4f73fdf3
Jochizan <remnyachizot2015@gmail.com>
2021-08-30 21:38:18 -0500

    Finish intro hooks basics with react

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,7 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
+
+const initialState = {
+  favorites: []
+};
+
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITE':
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload]
+      };
+    default:
+      return state;
+  }
+};
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 
   useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character/')
@@ -9,10 +26,22 @@ const Characters = () => {
       .then((data) => setCharacters(data.results));
   }, []);
 
+  const handleClick = (favorite) => {
+    dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite });
+  };
+
   return (
     <div>
+      {favorites.favorites.map((favorite) => (
+        <li key={favorite.id}>{favorite.name}</li>
+      ))}
       {characters.map((el, idx) => (
-        <h2 key={idx}>{el.name}</h2>
+        <div className='item' key={idx}>
+          <h2>{el.name}</h2>
+          <button type='button' onClick={() => handleClick(el)}>
+            Agregar a favoritos
+          </button>
+        </div>
       ))}
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import ThemeContext from '../context/ThemeContext';
 
 const Header = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const color = useContext(ThemeContext);
 
   const handleClick = () => {
     setDarkMode(!darkMode);
@@ -9,7 +11,7 @@ const Header = () => {
 
   return (
     <div className='header'>
-      <h1>ReactHooks</h1>
+      <h1 style={{ color }}>ReactHooks</h1>
       <button type='button' onClick={handleClick}>
         {darkMode ? 'Dark' : 'Light'}
       </button>
